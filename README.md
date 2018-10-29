8b5bc9b9a3bee170a928ebb01cf9d77f114bd102
Christian <chris.heyn.dd@gmail.com>
2018-10-29 18:21:16 +0100

    renamed package

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# time-string
+# string-to-time
 
 ```
-const { timeString } = require('time-string);
+const { stringToTime } = require('string-to-time');
 
-const calculated = timeString('4hours for some stuff, 1.2days plus 2 x 35minutes for somthing else...');
+const calculated = stringToTime('4hours for some stuff, 1.2days plus 2 x 35minutes for somthing else...');
 console.log(calculated);
 // --> { d: 1, h: 6, m: 46 }
 ```
--- src/index.test.js
+++ src/index.test.js
@@ -1,14 +1,14 @@
-const { timeString } = require('../index');
+const { stringToTime } = require('../index');
 
-describe('timeString', () => {
+describe('stringToTime', () => {
     it('is a function', () => {
-        const actual = typeof timeString;
+        const actual = typeof stringToTime;
         const expected = 'function';
         expect(actual).toEqual(expected);
     });
 
     it('returns days and hours and minutes from string', () => {
-        const actual = timeString('4hours for some stuff, 1.2days plus 2 x 35minutes for somthing else...');
+        const actual = stringToTime('4hours for some stuff, 1.2days plus 2 x 35minutes for somthing else...');
         const expected = {
             d: 1,
             h: 6,
